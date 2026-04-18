2164280be89de45428bdce09f991555d85cd64c3
soykot49 <mdrezwannurrashid26@gmail.com>
2026-04-19 04:00:01 +0600

    data added

---
+++ src/data/ticketData.js
@@ -0,0 +1,112 @@
+export const ticketsData = [
+  {
+    
+    id: 1001,
+    title: "Login issues - Can't Access Account",
+    description:
+      "Customer is unable to log in to their account. They’ve tried resetting their password multiple times but still unable to access...",
+    customer: "Alan Smith",
+    priority: "HIGH PRIORITY",
+    status: "Open",
+    createdAt: "15/12/2024",
+  },
+
+  {
+    id: 1002,
+    title: "Payment Failed - Card Declined",
+    description:
+      "Customer attempted to pay using Visa ending 1234, but the payment keeps failing despite sufficient balance.",
+    customer: "Sarah Lith",
+    priority: "HIGH PRIORITY",
+    status: "Open",
+    createdAt: "15/12/2024",
+  },
+
+  {
+    id: 1003,
+    title: "Unable to Download Invoice",
+    description:
+      "Customer cannot download their January invoice from the billing section. The download button isn't responding.",
+    customer: "Lina Brown",
+    priority: "MEDIUM PRIORITY",
+    status: "In-Progress",
+    createdAt: "17/12/2024",
+  },
+
+  {
+    id: 1004,
+    title: "Incorrect Billing Address",
+    description:
+      "Customer’s billing address shows a different city. They updated it but it still displays the old one.",
+    customer: "Emilio Giovanni",
+    priority: "LOW PRIORITY",
+    status: "Open",
+    createdAt: "18/12/2024",
+  },
+
+  {
+    id: 1005,
+    title: "App Crash on Launch",
+    description:
+      "Customer reports that the mobile app crashes immediately upon opening on Android 13.",
+    customer: "David Malan",
+    priority: "HIGH PRIORITY",
+    status: "Open",
+    createdAt: "19/12/2024",
+  },
+
+  {
+    id: 1006,
+    title: "Refund Not Processed",
+    description:
+      "Customer requested a refund two weeks ago but has not received the amount yet.",
+    customer: "Sophie Willson",
+    priority: "MEDIUM PRIORITY",
+    status: "In-Progress",
+    createdAt: "12/12/2024",
+  },
+
+  {
+    id: 1007,
+    title: "Two-Factor Authentication Issue",
+    description:
+      "Customer is not receiving 2FA codes on their registered phone number.",
+    customer: "James A Kollingue",
+    priority: "HIGH PRIORITY",
+    status: "Open",
+    createdAt: "12/12/2024",
+  },
+
+  {
+    id: 1008,
+    title: "Unable to Update Profile Picture",
+    description:
+      "Customer tries to upload a new profile picture but gets 'Upload failed' error.",
+    customer: "Ronja Anya",
+    priority: "LOW PRIORITY",
+    status: "Open",
+    createdAt: "12/12/2024",
+  },
+
+  {
+    id: 1009,
+    title: "Subscription Auto-Renewal",
+    description:
+      "Customer wants to enable auto-renewal for their subscription but the toggle is disabled.",
+    customer: "Liam Shybill",
+    priority: "MEDIUM PRIORITY",
+    status: "In-Progress",
+    createdAt: "17/12/2024",
+  },
+
+  {
+    id: 1010,
+    title: "Missing Order Confirmation Email",
+    description:
+      "Customer placed an order but didn’t receive a confirmation email even though payment succeeded.",
+    customer: "Isabella Julia",
+    priority: "MEDIUM PRIORITY",
+    status: "Open",
+    createdAt: "12/12/2024",
+  },
+];
